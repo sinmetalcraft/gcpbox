aa7dfad991ee47756f402610813f67029ae2d633
sinmetal <sinmetal@sinmetalcraft.jp>
2020-07-10 20:44:54 +0900

    使う側が楽なようにFolderとProjectを内部で持った refs #20

--- cloudresourcemanager/resourcemanager_service.go
+++ cloudresourcemanager/resourcemanager_service.go
@@ -109,11 +109,170 @@ func (s *ResourceManagerService) ConvertIamMember(member string) (*IamMember, er
 	}
 }
 
+// Folder: A Folder in an Organization's resource hierarchy, used
+// to
+// organize that Organization's resources.
+type Folder struct {
+	// CreateTime: Output only. Timestamp when the Folder was created.
+	// Assigned by the server.
+	CreateTime string `json:"createTime,omitempty"`
+
+	// DisplayName: The folder’s display name.
+	// A folder’s display name must be unique amongst its siblings,
+	// e.g.
+	// no two folders with the same parent can share the same display
+	// name.
+	// The display name must start and end with a letter or digit, may
+	// contain
+	// letters, digits, spaces, hyphens and underscores and can be no
+	// longer
+	// than 30 characters. This is captured by the regular
+	// expression:
+	// [\p{L}\p{N}]([\p{L}\p{N}_- ]{0,28}[\p{L}\p{N}])?.
+	DisplayName string `json:"displayName,omitempty"`
+
+	// LifecycleState: Output only. The lifecycle state of the
+	// folder.
+	// Updates to the lifecycle_state must be performed via
+	// DeleteFolder and
+	// UndeleteFolder.
+	//
+	// Possible values:
+	//   "LIFECYCLE_STATE_UNSPECIFIED" - Unspecified state.
+	//   "ACTIVE" - The normal and active state.
+	//   "DELETE_REQUESTED" - The folder has been marked for deletion by the
+	// user.
+	LifecycleState string `json:"lifecycleState,omitempty"`
+
+	// Name: Output only. The resource name of the Folder.
+	// Its format is `folders/{folder_id}`, for example: "folders/1234".
+	Name string `json:"name,omitempty"`
+
+	// Parent: Required. The Folder’s parent's resource name.
+	// Updates to the folder's parent must be performed via
+	// MoveFolder.
+	Parent string `json:"parent,omitempty"`
+}
+
+// Project: A Project is a high-level Google Cloud Platform entity.  It
+// is a
+// container for ACLs, APIs, App Engine Apps, VMs, and other
+// Google Cloud Platform resources.
+type Project struct {
+	// CreateTime: Creation time.
+	//
+	// Read-only.
+	CreateTime string `json:"createTime,omitempty"`
+
+	// Labels: The labels associated with this Project.
+	//
+	// Label keys must be between 1 and 63 characters long and must
+	// conform
+	// to the following regular expression:
+	// \[a-z\](\[-a-z0-9\]*\[a-z0-9\])?.
+	//
+	// Label values must be between 0 and 63 characters long and must
+	// conform
+	// to the regular expression (\[a-z\](\[-a-z0-9\]*\[a-z0-9\])?)?. A
+	// label
+	// value can be empty.
+	//
+	// No more than 256 labels can be associated with a given
+	// resource.
+	//
+	// Clients should store labels in a representation such as JSON that
+	// does not
+	// depend on specific characters being disallowed.
+	//
+	// Example: <code>"environment" : "dev"</code>
+	// Read-write.
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// LifecycleState: The Project lifecycle state.
+	//
+	// Read-only.
+	//
+	// Possible values:
+	//   "LIFECYCLE_STATE_UNSPECIFIED" - Unspecified state.  This is only
+	// used/useful for distinguishing
+	// unset values.
+	//   "ACTIVE" - The normal and active state.
+	//   "DELETE_REQUESTED" - The project has been marked for deletion by
+	// the user
+	// (by invoking
+	// DeleteProject)
+	// or by the system (Google Cloud Platform).
+	// This can generally be reversed by invoking UndeleteProject.
+	//   "DELETE_IN_PROGRESS" - This lifecycle state is no longer used and
+	// not returned by the API.
+	LifecycleState string `json:"lifecycleState,omitempty"`
+
+	// Name: The optional user-assigned display name of the Project.
+	// When present it must be between 4 to 30 characters.
+	// Allowed characters are: lowercase and uppercase letters,
+	// numbers,
+	// hyphen, single-quote, double-quote, space, and exclamation
+	// point.
+	//
+	// Example: <code>My Project</code>
+	// Read-write.
+	Name string `json:"name,omitempty"`
+
+	// Parent: An optional reference to a parent Resource.
+	//
+	// Supported parent types include "organization" and "folder". Once set,
+	// the
+	// parent cannot be cleared. The `parent` can be set on creation or
+	// using the
+	// `UpdateProject` method; the end user must have
+	// the
+	// `resourcemanager.projects.create` permission on the
+	// parent.
+	//
+	// Read-write.
+	Parent *ResourceID `json:"parent,omitempty"`
+
+	// ProjectId: The unique, user-assigned ID of the Project.
+	// It must be 6 to 30 lowercase letters, digits, or hyphens.
+	// It must start with a letter.
+	// Trailing hyphens are prohibited.
+	//
+	// Example: <code>tokyo-rain-123</code>
+	// Read-only after creation.
+	ProjectID string `json:"projectId,omitempty"`
+
+	// ProjectNumber: The number uniquely identifying the project.
+	//
+	// Example: <code>415104041262</code>
+	// Read-only.
+	ProjectNumber int64 `json:"projectNumber,omitempty,string"`
+}
+
+// ResourceId: A container to reference an id for any resource type. A
+// `resource` in Google
+// Cloud Platform is a generic term for something you (a developer) may
+// want to
+// interact with through one of our API's. Some examples are an App
+// Engine app,
+// a Compute Engine instance, a Cloud SQL database, and so on.
+type ResourceID struct {
+	// Id: Required field for the type-specific id. This should correspond
+	// to the id
+	// used in the type-specific API's.
+	ID string `json:"id,omitempty"`
+
+	// Type: Required field representing the resource type this id is
+	// for.
+	// At present, the valid types are: "organization", "folder", and
+	// "project".
+	Type string `json:"type,omitempty"`
+}
+
 // Folders 指定した parent の下にあるすべてのFolderを返す
 // 階層構造は保持せずにフラットにすべてのFolderを返す
 // parent は `folders/{folder_id}` or `organizations/{org_id}` の形式で指定する
-func (s *ResourceManagerService) Folders(ctx context.Context, parent string) ([]*crmv2.Folder, error) {
-	var folders []*crmv2.Folder
+func (s *ResourceManagerService) Folders(ctx context.Context, parent string) ([]*Folder, error) {
+	var folders []*Folder
 	var err error
 	folders, err = s.folders(ctx, parent, folders)
 	if err != nil {
@@ -122,7 +281,7 @@ func (s *ResourceManagerService) Folders(ctx context.Context, parent string) ([]
 	return folders, nil
 }
 
-func (s *ResourceManagerService) folders(ctx context.Context, parent string, dst []*crmv2.Folder) ([]*crmv2.Folder, error) {
+func (s *ResourceManagerService) folders(ctx context.Context, parent string, dst []*Folder) ([]*Folder, error) {
 	req := s.crmv2.Folders.List().Parent(parent)
 	if err := req.Pages(ctx, func(page *crmv2.ListFoldersResponse) error {
 		for _, folder := range page.Folders {
@@ -130,7 +289,12 @@ func (s *ResourceManagerService) folders(ctx context.Context, parent string, dst
 			if err != nil {
 				return err
 			}
-			dst = append(dst, folder)
+			dst = append(dst, &Folder{
+				Name:           folder.Name,
+				Parent:         folder.Parent,
+				LifecycleState: folder.LifecycleState,
+				CreateTime:     folder.CreateTime,
+			})
 			dst = append(dst, l...)
 		}
 		return nil
@@ -142,16 +306,31 @@ func (s *ResourceManagerService) folders(ctx context.Context, parent string, dst
 }
 
 // Projects is 指定したリソース以下のProject一覧を返す
-func (s *ResourceManagerService) Projects(ctx context.Context, parentID string) ([]*crmv1.Project, error) {
+func (s *ResourceManagerService) Projects(ctx context.Context, parentID string) ([]*Project, error) {
 	req := s.crmv1.Projects.List()
 	if len(parentID) > 0 {
 		req.Filter(fmt.Sprintf("parent.id:%s", parentID))
 	}
 
-	var list []*crmv1.Project
+	var list []*Project
 	if err := req.Pages(ctx, func(page *crmv1.ListProjectsResponse) error {
 		for _, project := range page.Projects {
-			list = append(list, project)
+			p := &Project{
+				ProjectID:      project.ProjectId,
+				ProjectNumber:  project.ProjectNumber,
+				Name:           project.Name,
+				LifecycleState: project.LifecycleState,
+				Labels:         project.Labels,
+				CreateTime:     project.CreateTime,
+			}
+			if project.Parent != nil {
+				p.Parent = &ResourceID{
+					ID:   project.Parent.Id,
+					Type: project.Parent.Type,
+				}
+			}
+
+			list = append(list, p)
 		}
 		return nil
 	}); err != nil {
