82c321938b9bb1dd235bbc13553972a7c8859cf6
sinmetal <sinmetal@sinmetalcraft.jp>
2020-10-19 21:15:32 +0900

    add cloudtasks http target task service

---
+++ cloudtasks/service.go
@@ -0,0 +1,125 @@
+package cloudtasks
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	"golang.org/x/xerrors"
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+)
+
+// Service is Cloud Tasks Service
+type Service struct {
+	taskClient          *cloudtasks.Client
+	serviceAccountEmail string
+}
+
+// NewService is return Service
+func NewService(ctx context.Context, taskClient *cloudtasks.Client, serviceAccountEmail string) (*Service, error) {
+	return &Service{
+		taskClient:          taskClient,
+		serviceAccountEmail: serviceAccountEmail,
+	}, nil
+}
+
+// Queue is Cloud Tasks Queue
+type Queue struct {
+	ProjectID string
+	Region    string
+	Name      string
+}
+
+// Parent is return Cloud Tasks Parent format value
+func (q *Queue) Parent() string {
+	return fmt.Sprintf("projects/%s/locations/%s/queues/%s", q.ProjectID, q.Region, q.Name)
+}
+
+// JsonPostTask is JsonをBodyに入れるTask
+type JsonPostTask struct {
+	// OIDC の Audience
+	//
+	// IAPに向けて投げる時は、IAPのClient IDを指定する
+	// https://cloud.google.com/iap/docs/authentication-howto#authenticating_from_a_service_account
+	//
+	// Cloud Run.Invokerに投げる場合は RelativeUri と同じものを指定する
+	Audience string
+
+	// Task が到達する Handler の URL
+	RelativeUri string
+
+	// Task Body
+	// 中で JSON に変換する
+	Body interface{}
+}
+
+// CreateJsonPostTask is BodyにJsonを入れるTaskを作る
+func (s *Service) CreateJsonPostTask(ctx context.Context, queue *Queue, task *JsonPostTask) (string, error) {
+	body, err := json.Marshal(task.Body)
+	if err != nil {
+		return "", xerrors.Errorf("failed json.Marshal(). body=%+v : %w", task.Body, err)
+	}
+	got, err := s.taskClient.CreateTask(ctx, &taskspb.CreateTaskRequest{
+		Parent: queue.Parent(),
+		Task: &taskspb.Task{
+			MessageType: &taskspb.Task_HttpRequest{
+				HttpRequest: &taskspb.HttpRequest{
+					Url:        task.RelativeUri,
+					Headers:    map[string]string{"Content-Type": "application/json"},
+					HttpMethod: taskspb.HttpMethod_POST,
+					Body:       body,
+					AuthorizationHeader: &taskspb.HttpRequest_OidcToken{OidcToken: &taskspb.OidcToken{
+						ServiceAccountEmail: s.serviceAccountEmail,
+						Audience:            task.Audience,
+					}},
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", xerrors.Errorf("failed CreateJsonPostTask(). queue=%+v, body=%+v : %w", queue, task.Body, err)
+	}
+	return got.Name, nil
+}
+
+// GetTask is Get Request 用の Task
+type GetTask struct {
+	// OIDC の Audience
+	//
+	// IAPに向けて投げる時は、IAPのClient IDを指定する
+	// https://cloud.google.com/iap/docs/authentication-howto#authenticating_from_a_service_account
+	//
+	// Cloud Run.Invokerに投げる場合は RelativeUri と同じものを指定する
+	Audience string
+
+	// Task Request の Header
+	Headers map[string]string
+
+	// Task が到達する Handler の URL
+	RelativeUri string
+}
+
+// CreateGetTask is Get Request 用の Task を作る
+func (s *Service) CreateGetTask(ctx context.Context, queue *Queue, task *GetTask) (string, error) {
+	got, err := s.taskClient.CreateTask(ctx, &taskspb.CreateTaskRequest{
+		Parent: queue.Parent(),
+		Task: &taskspb.Task{
+			MessageType: &taskspb.Task_HttpRequest{
+				HttpRequest: &taskspb.HttpRequest{
+					Url:        task.RelativeUri,
+					Headers:    task.Headers,
+					HttpMethod: taskspb.HttpMethod_GET,
+					AuthorizationHeader: &taskspb.HttpRequest_OidcToken{OidcToken: &taskspb.OidcToken{
+						ServiceAccountEmail: s.serviceAccountEmail,
+						Audience:            task.Audience,
+					}},
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", xerrors.Errorf("failed CreateJsonPostTask(). queue=%+v, url=%s : %w", queue, task.RelativeUri, err)
+	}
+	return got.Name, nil
+}
---
+++ cloudtasks/service_test.go
@@ -0,0 +1,56 @@
+package cloudtasks_test
+
+import (
+	"context"
+	"testing"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	tasksbox "github.com/sinmetalcraft/gcpbox/cloudtasks"
+	metadatabox "github.com/sinmetalcraft/gcpbox/metadata"
+)
+
+func TestService_CreateJsonPostTask(t *testing.T) {
+	ctx := context.Background()
+
+	s := newService(t)
+
+	queue := &tasksbox.Queue{
+		ProjectID: "sinmetal-ci",
+		Region:    "asia-northeast1",
+		Name:      "gcpboxtest",
+	}
+	type Body struct {
+		Content string
+	}
+	taskName, err := s.CreateJsonPostTask(ctx, queue, &tasksbox.JsonPostTask{
+		RelativeUri: "",
+		Body: &Body{
+			Content: "Hello JsonPostTask",
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(taskName) < 1 {
+		t.Error("task name is empty")
+	}
+}
+
+func newService(t *testing.T) *tasksbox.Service {
+	ctx := context.Background()
+
+	taskClient, err := cloudtasks.NewClient(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sa, err := metadatabox.ServiceAccountEmail()
+	if err != nil {
+		t.Fatal(err)
+	}
+	s, err := tasksbox.NewService(ctx, taskClient, sa)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return s
+}
