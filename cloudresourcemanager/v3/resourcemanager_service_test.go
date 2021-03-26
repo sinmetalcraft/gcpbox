f4ca12577b29094be1e4d0a8db78e9cecbfb2529
sinmetal <sinmetal@sinmetalcraft.jp>
2021-03-26 16:21:05 +0900

    fix

--- cloudresourcemanager/v3/resourcemanager_service_test.go
+++ cloudresourcemanager/v3/resourcemanager_service_test.go
@@ -221,7 +221,7 @@ func TestResourceManagerService_ExistsMemberInGCPProjectWithInherit(t *testing.T
 		{"Projectが存在して権限を持っているが、親のFolderの権限をAppが持っていない", "gentle-mapper-229103", "hoge@example.com", nil, false, crmbox.ErrPermissionDenied},
 		{"Projectが存在して権限を持っているが、親のOrganizationの権限をAppが持っていない", "gentle-mapper-229103", "hoge@example.com", nil, false, crmbox.ErrPermissionDenied},
 		{"Projectが存在して権限を持っており、メンバーが存在していない", "sinmetal-ci", "hoge@example.com", nil, false, nil},
-		{"Projectが存在して権限を持っていない", "gcpug-public-spanner", "hoge@example.com", nil, false, crmbox.ErrPermissionDenied},
+		{"Projectが存在して権限を持っていない", "gcpug-public-spanner", "hoge@example.com", nil, false, nil},
 		{"Projectが存在していない", "adoi893lda3fd1", "hoge@example.com", nil, false, crmbox.ErrPermissionDenied},
 		{"Projectが存在して、Projectが所属している祖父のFolderの権限を持っているメンバーが存在しているが、step数的に届かない", "gcpbox-ci", "gcpbox-iam-test-1@sinmetal-ci.iam.gserviceaccount.com", []crmbox.ExistsMemberInheritOptions{crmbox.WithStep(1)}, false, nil},
 		{"Projectが存在して、Projectが所属しているOrganizationの権限を持っているメンバーが存在しているがOrganizationまで見に行かない", "gcpbox-ci", "gcpbox-iam-test-2@sinmetal-ci.iam.gserviceaccount.com", []crmbox.ExistsMemberInheritOptions{crmbox.WithTopNode(&crmbox.ResourceID{Type: "folder", ID: "484650900491"})}, false, nil},
