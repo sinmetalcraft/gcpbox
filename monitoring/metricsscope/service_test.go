2a17244e8a6bcf8f260d715f2f1c89ce16c5c160
sinmetal <sinmetal@sinmetalcraft.jp>
2023-01-31 15:52:57 +0900

    コケにくいようにテストを修正

--- monitoring/metricsscope/service_test.go
+++ monitoring/metricsscope/service_test.go
@@ -56,10 +56,14 @@ func TestService_CreateMonitoredProject(t *testing.T) {
 
 	const monitoredProject = "sinmetal"
 	got, err := s.CreateMonitoredProject(ctx, project, monitoredProject)
-	if err != nil {
+	if status.Code(err) == codes.AlreadyExists {
+		// すでに存在してい場合はスルー
+		t.Logf("%s is Already Exists in metrics scope", monitoredProject)
+	} else if err != nil {
 		t.Fatal(err)
+	} else {
+		t.Logf("got MonitoredProject:%s\n", got.GetName())
 	}
-	t.Logf("got MonitoredProject:%s\n", got.GetName())
 
 	// すでに存在する場合はAlreadyExistsが返ってくる
 	_, err = s.CreateMonitoredProject(ctx, project, monitoredProject)
@@ -67,6 +71,7 @@ func TestService_CreateMonitoredProject(t *testing.T) {
 		t.Errorf("want AlreadyExists but got %v", err)
 	}
 
+	// 次回テストの時のために削除しておく
 	if err := s.DeleteMonitoredProject(ctx, project, monitoredProject); err != nil {
 		t.Fatal(err)
 	}
